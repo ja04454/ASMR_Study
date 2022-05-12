824355b75cb2bce699b5342dd554eba190613f20
sugarghost <code.sugartoast@gmail.com>
2022-05-12 16:04:16 +0900

    서버에서 500오류가 발생함, 구글링 결과 서버에서 utf-8로 별도 디코딩이 필요한 경우가 있다는 자료가 있어 공유 후 테스트

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def sign_in():
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         # SECRET_KEY로 암호화
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
         # 토큰을 준다
         return jsonify({'result': 'success', 'token': token})
